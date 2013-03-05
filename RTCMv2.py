746929617861aa32a02d8c51d91abf375331753a
Andrew Tridgell <tridge@samba.org>
2013-03-05 15:52:42 +1100

    RTCMv2 cleanup

--- RTCMv2.py
+++ RTCMv2.py
@@ -3,26 +3,31 @@ import struct, util
 class RTCMBits:
     '''RTCMv2 bit packer. Thanks to Michael Oborne for the C# code this was based on
 
+    Also thanks to this site for a great RTCMv2 parser to test the generated output
     ftp://ftp.tapr.org/gps/DGPS/N8PXW/
-'''
+    '''
     def __init__(self):
         self.rtcmseq = 0
-        self.flip = [0,32,16,48,8,40,24,56,4,36,20,52,12,44,28,60,
-                     2,34,18,50,10,42,26,58,6,38,22,54,14,46,30,62,
-                     1,33,17,49,9,41,25,57,5,37,21,53,13,45,29,61,
-                     3,35,19,51,11,43,27,59,7,39,23,55,15,47,31,63]
-        self.parity29 = 0
-        self.parity30 = 0
+        self.bitreverse = self.bitreverse_array()
+        self.parity1 = 0
+        self.parity2 = 0
         self.reset()
         self.error_history = {}
         self.history_length = 10
         self.last_time_of_week = 0
+        self.stationID = 2
 
     def reset(self):
         '''reset at the end of a message'''
         self.buf = ""
         self.rtcbits = 0
         self.rtcword = 0
+
+    def bitreverse_array(self):
+        ret = []
+        for i in range(64):
+            ret.append(int('{:06b}'.format(i)[::-1], 2))
+        return ret
 
     def addbits(self, nbits, value):
         '''add nbits bits of value to the buffer'''
@@ -44,17 +49,17 @@ class RTCMBits:
         # move data into lower 30 bits of 32 bit word
         self.rtcword <<= 6
 
-        i = self.doparity(self.rtcword)
+        i = self.calculate_parity(self.rtcword)
         # put parity into lower 6 bits of 32 bit word
         self.rtcword |= i
         # invert bits, if needed 
-        if self.parity30 != 0:
+        if self.parity2 != 0:
             self.rtcword ^= 0x3fffffc0
         # copy parity bits  */
-        self.parity30 = self.rtcword & 1
-        self.parity29 = 0
+        self.parity2 = self.rtcword & 1
+        self.parity1 = 0
         if (self.rtcword & 2) != 0:
-            self.parity29 = 1
+            self.parity1 = 1
 
         rtcbufr = [0]*5
 
@@ -62,8 +67,8 @@ class RTCMBits:
             #* extract bottom 6 bits
             rtcbufr[i] = self.rtcword & 0x3f
             self.rtcword >>= 6
-            # flip the bit order
-            rtcbufr[i] = self.flip[rtcbufr[i]]
+            # reverse the bit order
+            rtcbufr[i] = self.bitreverse[rtcbufr[i]]
             # or in "01" into the upper bits
             rtcbufr[i] |= 0x40
 
@@ -72,40 +77,29 @@ class RTCMBits:
         self.rtcword = 0
         self.rtcbits = 0
 
-    def getbit(self, word, bit):
-        return (word >> (29 - bit)) & 1
+    def xor_bits(self, word, bits):
+        '''xor a set of bits from a word'''
+        ret = 0
+        for b in bits:
+            ret ^= (word >> (29 - b)) & 1
+        return ret
 
-    def doparity(self, word):
-        d25 = d26 = d27 = d28 = d29 = d30 = 0
-        parity29 = self.parity29
-        parity30 = self.parity30
+    def calculate_parity(self, word):
+        '''calculate 6 parity bits for a word'''
+        parity1 = self.parity1
+        parity2 = self.parity2
 
-        d25 = parity29 ^ self.getbit(word, 0) ^ self.getbit(word, 1) ^ self.getbit(word, 2) ^ \
-            self.getbit(word, 4) ^ self.getbit(word, 5) ^ self.getbit(word, 9) ^ self.getbit(word, 10) ^ \
-            self.getbit(word, 11) ^ self.getbit(word, 12) ^ self.getbit(word, 13) ^ self.getbit(word, 16) ^ \
-            self.getbit(word, 17) ^ self.getbit(word, 19) ^ self.getbit(word, 22)
-        d26 = parity30 ^ self.getbit(word, 1) ^ self.getbit(word, 2) ^ self.getbit(word, 3) ^ \
-            self.getbit(word, 5) ^ self.getbit(word, 6) ^ self.getbit(word, 10) ^ self.getbit(word, 11) ^ \
-            self.getbit(word, 12) ^ self.getbit(word, 13) ^ self.getbit(word, 14) ^ self.getbit(word, 17) ^ \
-            self.getbit(word, 18) ^ self.getbit(word, 20) ^ self.getbit(word, 23)
-        d27 = parity29 ^ self.getbit(word, 0) ^ self.getbit(word, 2) ^ self.getbit(word, 3) ^ \
-            self.getbit(word, 4) ^ self.getbit(word, 6) ^ self.getbit(word, 7) ^ self.getbit(word, 11) ^ \
-            self.getbit(word, 12) ^ self.getbit(word, 13) ^ self.getbit(word, 14) ^ self.getbit(word, 15) ^ \
-            self.getbit(word, 18) ^ self.getbit(word, 19) ^ self.getbit(word, 21)
-        d28 = parity30 ^ self.getbit(word, 1) ^ self.getbit(word, 3) ^ self.getbit(word, 4) ^ \
-            self.getbit(word, 5) ^ self.getbit(word, 7) ^ self.getbit(word, 8) ^ self.getbit(word, 12) ^ \
-            self.getbit(word, 13) ^ self.getbit(word, 14) ^ self.getbit(word, 15) ^ self.getbit(word, 16) ^ \
-            self.getbit(word, 19) ^ self.getbit(word, 20) ^ self.getbit(word, 22)
-        d29 = parity30 ^ self.getbit(word, 0) ^ self.getbit(word, 2) ^ self.getbit(word, 4) ^ \
-            self.getbit(word, 5) ^ self.getbit(word, 6) ^ self.getbit(word, 8) ^ self.getbit(word, 9) ^ \
-            self.getbit(word, 13) ^ self.getbit(word, 14) ^ self.getbit(word, 15) ^ self.getbit(word, 16) ^ \
-            self.getbit(word, 17) ^ self.getbit(word, 20) ^ self.getbit(word, 21) ^ self.getbit(word, 23)
-        d30 = parity29 ^ self.getbit(word, 2) ^ self.getbit(word, 4) ^ self.getbit(word, 5) ^ \
-            self.getbit(word, 7) ^ self.getbit(word, 8) ^ self.getbit(word, 9) ^ self.getbit(word, 10) ^ \
-            self.getbit(word, 12) ^ self.getbit(word, 14) ^ self.getbit(word, 18) ^ self.getbit(word, 21) ^ \
-            self.getbit(word, 22) ^ self.getbit(word, 23)
-        parity = (((((((((d25 << 1) + d26) << 1) + d27) << 1) + d28) << 1) + d29) << 1) + d30
-        return parity
+        d = [0]*6
+        d[0] = parity1 ^ self.xor_bits(word, [0, 1, 2, 4, 5,  9, 10, 11, 12, 13, 16, 17, 19, 22])
+        d[1] = parity2 ^ self.xor_bits(word, [1, 2, 3, 5, 6, 10, 11, 12, 13, 14, 17, 18, 20, 23])
+        d[2] = parity1 ^ self.xor_bits(word, [0, 2, 3, 4, 6,  7, 11, 12, 13, 14, 15, 18, 19, 21])
+        d[3] = parity2 ^ self.xor_bits(word, [1, 3, 4, 5, 7,  8, 12, 13, 14, 15, 16, 19, 20, 22])
+        d[4] = parity2 ^ self.xor_bits(word, [0, 2, 4, 5, 6,  8,  9, 13, 14, 15, 16, 17, 20, 21, 23])
+        d[5] = parity1 ^ self.xor_bits(word, [2, 4, 5, 7, 8,  9, 10, 12, 14, 18, 21, 22, 23])
+        ret = d[0]
+        for i in range(1, 6):
+            ret = (ret<<1) + d[i]
+        return ret
         
 
     def RTCMType1(self, satinfo):
@@ -145,56 +139,43 @@ class RTCMBits:
             msgiode.append(satinfo.ephemeris[svid].iode)
 
 
-#        print("msgsatid:", msgsatid)
-#        print("msgprc:", msgprc)
-#        print("msgiode:", msgiode)
-            
         rtcmzcount = int((int(tow) % 3600) / 0.6)
 
+        # first part of header
         self.addbits(8, 0x66)  # header id
         self.addbits(6, 1)     # msg type 1
-        self.addbits(10, 2)    # station id
+        self.addbits(10, self.stationID) 
 
-        #  1st word should be sent here
+        #  second part of header
         self.addbits(13, rtcmzcount) # z-count
         self.addbits(3, self.rtcmseq) # seq no.
         self.rtcmseq = (self.rtcmseq + 1) % 8
 
-        # now compute the word count
-        i = msgsatcnt * 40 #  no of bits to send
-        j = i // 24  # no of RTCM words to send
-        if (i % 24) != 0:
-            j += 1  # bump, if not a full word
-        bitlength = i
-        wordlength = j
+        # now compute the word length of the message
+        # each word contains 24 bits of data, plus 6 bits of parity
+        bitlength = msgsatcnt * 40
+        wordlength = bitlength // 24
+        if (bitlength % 24) != 0:
+            wordlength += 1
 
-        self.addbits(5, wordlength) # word length
-        self.addbits(3, 0) # health bits
+        self.addbits(5, wordlength)
+        # health bits - mark as healthy
+        self.addbits(3, 0)
 
         for i in range(msgsatcnt):
             '''      
             We have to calculate the scale factor, first.
-            Mot PRC is 24 bits, scaled by 0.01 .
             RTCM PRC is 16 bits, scaled by 0.02, or 0.32 if sf==1 .
-            Mot PRRC is 16 bits, scaled by 0.001 .
             RTCM PRRC is 8 bits, scaled by 0.002, or 0.032 if sf==1 .
             '''
-            # 1. divide PRC and PRRC by 2 to get base corrections 
-            # msgprc[i] /= 2
-            # msgprrc[i] /= 2
             sf = 0
-            # 2. see if PRC > 16 bits, or PRRC > 8 bits, if so sf=1.
             if msgprc[i] > 32767 or msgprc[i] < -32768:
                 sf = 1
             if msgprrc[i] > 127 or msgprrc[i] < -128:
                 sf = 1
-            # 3a. if sf==1, add 8 to both PRC and PRRC to round up
-            # 3b. if sf==1, then divide both PRC and PRRC by 16.
             if sf != 0:
-                msgprc[i] += 8
-                msgprrc[i] += 8
-                msgprc[i] /= 16
-                msgprrc[i] /= 16
+                msgprc[i] = (msgprc[i] + 8) // 16
+                msgprrc[i] = (msgprrc[i] + 8) // 16
             self.addbits(1, sf) # scale factor
             self.addbits(2, 0)  # UDRE
             self.addbits(5, msgsatid[i]) # sat id no
