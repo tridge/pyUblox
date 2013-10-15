03b838d663a42b3efd27d5fcd9a218ac14247f34
Ben Nizette <benjamin.nizette@anu.edu.au>
2013-10-15 16:36:01 +1100

    Add a python-coded ntrip client that spits its output to stdout; works as a drop-in replacement for the reference POSIX implementation

---
+++ ntrip.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+import socket
+import base64
+import sys
+
+dummy, server, port, username, password, mountpoint = sys.argv
+
+pwd = base64.b64encode("{}:{}".format(username, password))
+
+header =\
+"GET /{} HTTP/1.1\r\n".format(mountpoint) +\
+"Host \r\n".format(server) +\
+"Ntrip-Version: Ntrip/2.0\r\n" +\
+"User-Agent: NTRIP NtripClientPOSIX/1.50\r\n" +\
+"Connection: close\r\n" +\
+"Authorization: Basic {}\r\n\r\n".format(pwd)
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((server,int(port)))
+s.send(header)
+
+resp = s.recv(1024)
+
+if resp.startswith("STREAMTABLE"):
+    raise NTRIPError("Invalid or No Mountpoint")
+elif not resp.startswith("HTTP/1.1 200 OK"):
+    raise NTRIPError("Invalid Server Response")
+
+try:
+    while True:
+        #length = s.recv(4)
+
+        #try:
+        #    length = int(length.strip(), 16)
+        #except ValueError:
+        #    continue
+
+        data = s.recv(1024)
+        print(data)
+        #print >>sys.stderr, [ord(d) for d in data]
+        sys.stdout.flush()
+
+finally:
+    s.close()
+
+
