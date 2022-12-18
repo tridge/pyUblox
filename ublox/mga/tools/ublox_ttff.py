7cf580df266eaf5291fc502e29923dd1c7bf1c9b
Peter Barker <pbarker@barker.dropbear.id.au>
2022-12-18 15:24:20 +1100

    stuff

--- ublox/mga/tools/ublox_ttff.py
+++ ublox/mga/tools/ublox_ttff.py
@@ -26,7 +26,6 @@ class TTFF(ublox.mga.tool.MGATool):
         self.state_fix_acquired = 18
         self.state_first_configure = 19
         self.state_uploading_mga_dbd = 20
-        self.state_waiting_reset = 21
         self.state_uploading_time = 22
         self.state_uploading_position = 23
         self.state_consider_aiding = 24
@@ -70,7 +69,7 @@ class TTFF(ublox.mga.tool.MGATool):
         self.dev.configure_message_rate(ublox.CLASS_NAV, ublox.MSG_NAV_PVT, 1)
         self.dev.configure_poll(ublox.CLASS_CFG, ublox.MSG_CFG_NAVX5)
 
-    def get_fix(self, mga_dbd_source_dev=None, mga_offline_cache_file=None, position=None):
+    def get_fix(self, mga_dbd_source_dev=None, mga_offline_cache_file=None, position=None, reset=False, send_time=False):
         self.state = self.state_deathly_cold
 
         lat = 0
@@ -80,8 +79,9 @@ class TTFF(ublox.mga.tool.MGATool):
         while self.state != self.state_fix_acquired:
             now = time.time()
             if self.state == self.state_deathly_cold:
-                print("resetting module")
-                self.dev.module_reset(0xffff, 0x00)
+                if reset:
+                    print("resetting module")
+                    self.dev.module_reset(0xffff, 0x00)
                 self.state = self.state_first_configure
                 # fall through....
             if self.state == self.state_first_configure:
@@ -108,26 +108,22 @@ class TTFF(ublox.mga.tool.MGATool):
                         if msg.iTOW > 2000:
                             self.state = self.state_first_configure
                             continue
-                    self.state = self.state_consider_aiding
+                    self.state = self.state_configure_ack_aiding
                 else:
                     self.configure_dev()
                 # fall through
-            if self.state == self.state_consider_aiding:
-                if (mga_dbd_source_dev is not None
-                    or mga_offline_cache_file is not None):
-                    self.state = self.state_configure_ack_aiding
-                else:
-                    self.state = self.state_waiting_for_nav_status
             if self.state == self.state_configure_ack_aiding:
-                print("Configuring ack-aiding")
+                print("######### Configuring ack-aiding")
                 self.configure_dev_ack_aiding()
                 self.state = self.state_uploading_time
 #                self.state = self.state_uploading_mga_dbd
                 # fall through
             if self.state == self.state_uploading_time:
-                # perhaps we should wait until ack-aiding is set
-                # before doing this, and wait for an ack....
-                self.sendtime() # this blocks for ack
+                if send_time:
+                    print("######### Sending time")
+                    # perhaps we should wait until ack-aiding is set
+                    # before doing this, and wait for an ack....
+                    self.sendtime() # this blocks for ack
                 self.state = self.state_uploading_position
             if self.state == self.state_uploading_position:
                 # perhaps we should wait until ack-aiding is set
@@ -171,6 +167,8 @@ class TTFF(ublox.mga.tool.MGATool):
                           help="serial baud rate", default=9600)
         parser.add_option("--mga-dbd", help="mga dbd data file", default=None)
         parser.add_option("--mga-offline", help="mga offline cache file", default=None)
+        parser.add_option("--reset", help="reset module time", action="store_true")
+        parser.add_option("--time", help="Set module time", action="store_true")
         parser.add_option("--position", help="position e.g. 149.0420946,-35.2103418,652.954", default=None)
         (opts, args) = parser.parse_args()
 
@@ -198,7 +196,8 @@ class TTFF(ublox.mga.tool.MGATool):
             split_position = tuple([ float(x) for x in split_position])
         self.get_fix(mga_dbd_source_dev=dbd_source_dev,
                      mga_offline_cache_file=opts.mga_offline,
-                     position=split_position)
+                     position=split_position,
+                     reset=opts.reset)
 
 
 ttff = TTFF()
